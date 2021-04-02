5b1becb56140585d365a6ce086297bd7cf8cd9b6
Tam Phuc <phannguyenpro085@gmail.com>
2021-04-03 00:50:10 +0700

    xoa nham file client, up lai

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { Component } from "react";
+//import { Container, AppBar, Typography } from "@material-ui/core";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./components/login/login";
+import Home from "./Home/home";
 
-const  App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
+export default class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path={"/"} component={Login}/>
+                        <Route exact path={"/home"} component={Home}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
 }
-
-export default App;
