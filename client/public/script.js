7f523847327983ac58a62b3d38cca40b20676366
Nazmer <kson5011@gmail.com>
2021-05-15 08:19:16 +0700

    fix: home about load script

--- client/public/script.js
+++ client/public/script.js
@@ -1,39 +1,39 @@
 // Get Color Attribute
 // Set the background book color
-$("li.book-item").each(function() {
+$("li.book-item").each(function () {
   var $this = $(this);
 
-  $this.find(".bk-front > div").css('background-color', $(this).data("color"));
-  $this.find(".bk-left").css('background-color', $(this).data("color"));
-  $this.find(".back-color").css('background-color', $(this).data("color"));
+  $this.find(".bk-front > div").css("background-color", $(this).data("color"));
+  $this.find(".bk-left").css("background-color", $(this).data("color"));
+  $this.find(".back-color").css("background-color", $(this).data("color"));
 
-  $this.find(".item-details a.button").on('click', function() {
+  $this.find(".item-details a.button").on("click", function () {
     displayBookDetails($this);
   });
 });
 
 function displayBookDetails(el) {
   var $this = $(el);
-  $('.main-container').addClass('prevent-scroll');
-  $('.main-overlay').fadeIn();
+  $(".main-container").addClass("prevent-scroll");
+  $(".main-overlay").fadeIn();
 
-  $this.find('.overlay-details').clone().prependTo('.main-overlay');
+  $this.find(".overlay-details").clone().prependTo(".main-overlay");
 
-  $('a.close-overlay-btn').on('click', function(e) {
+  $("a.close-overlay-btn").on("click", function (e) {
     e.preventDefault();
-    $('.main-container').removeClass('prevent-scroll');
-    $('.main-overlay').fadeOut();
-    $('.main-overlay').find('.overlay-details').remove();
+    $(".main-container").removeClass("prevent-scroll");
+    $(".main-overlay").fadeOut();
+    $(".main-overlay").find(".overlay-details").remove();
   });
 
-  $('.main-overlay a.preview').on('click', function() {
-    $('.main-overlay .overlay-desc').toggleClass('activated');
-    $('.main-overlay .overlay-preview').toggleClass('activated');
+  $(".main-overlay a.preview").on("click", function () {
+    $(".main-overlay .overlay-desc").toggleClass("activated");
+    $(".main-overlay .overlay-preview").toggleClass("activated");
   });
 
-  $('.main-overlay a.back-preview-btn').on('click', function() {
-    $('.main-overlay .overlay-desc').toggleClass('activated');
-    $('.main-overlay .overlay-preview').toggleClass('activated');
+  $(".main-overlay a.back-preview-btn").on("click", function () {
+    $(".main-overlay .overlay-desc").toggleClass("activated");
+    $(".main-overlay .overlay-preview").toggleClass("activated");
   });
 }
 
@@ -41,17 +41,17 @@ function displayBookDetails(el) {
  *  Offcanvas Menu
  */
 // Open Offcanvas Menu
-$('.main-navigation a').on('click', function() {
-  $('.main-container').addClass('nav-menu-open');
-  $('.main-overlay').fadeIn();
+$(".main-navigation a").on("click", function () {
+  $(".main-container").addClass("nav-menu-open");
+  $(".main-overlay").fadeIn();
 });
 
 // Close Offcanvas Menu
-$('.overlay-full').on('click', function() {
-  $('.main-container').removeClass('nav-menu-open');
-  $('.main-container').removeClass('prevent-scroll');
+$(".overlay-full").on("click", function () {
+  $(".main-container").removeClass("nav-menu-open");
+  $(".main-container").removeClass("prevent-scroll");
   $(this).parent().fadeOut();
-  $(this).parent().find('.overlay-details').remove();
+  $(this).parent().find(".overlay-details").remove();
 });
 
 /*
@@ -61,72 +61,71 @@ $('.overlay-full').on('click', function() {
 // Initiate Shuffle.js
 var Shuffle = window.shuffle;
 
-var bookList = function(element) {
+var bookList = function (element) {
   this.element = element;
 
   this.shuffle = new Shuffle(element, {
-    itemSelector: '.book-item',
+    itemSelector: ".book-item",
   });
 
   this._activeFilters = [];
   this.addFilterButtons();
   this.addSorting();
   this.addSearchFilter();
-  this.mode = 'exclusive';
+  this.mode = "exclusive";
 };
 
-bookList.prototype.toArray = function(arrayLike) {
+bookList.prototype.toArray = function (arrayLike) {
   return Array.prototype.slice.call(arrayLike);
 };
 
 // Catagory Filter Functions
 // Toggle mode for the Catagory filters
-bookList.prototype.toggleMode = function() {
-  if (this.mode === 'additive') {
-    this.mode = 'exclusive';
+bookList.prototype.toggleMode = function () {
+  if (this.mode === "additive") {
+    this.mode = "exclusive";
   } else {
-    this.mode = 'additive';
+    this.mode = "additive";
   }
 };
 
 // Filter buttons eventlisteners
-bookList.prototype.addFilterButtons = function() {
-  var options = document.querySelector('.filter-options');
+bookList.prototype.addFilterButtons = function () {
+  var options = document.querySelector(".filter-options");
   if (!options) {
     return;
   }
   var filterButtons = this.toArray(options.children);
 
-  filterButtons.forEach(function(button) {
-    button.addEventListener('click', this._handleFilterClick.bind(this), false);
+  filterButtons.forEach(function (button) {
+    button.addEventListener("click", this._handleFilterClick.bind(this), false);
   }, this);
 };
 
 // Function for the Catagory Filter
-bookList.prototype._handleFilterClick = function(evt) {
+bookList.prototype._handleFilterClick = function (evt) {
   var btn = evt.currentTarget;
-  var isActive = btn.classList.contains('active');
-  var btnGroup = btn.getAttribute('data-group');
+  var isActive = btn.classList.contains("active");
+  var btnGroup = btn.getAttribute("data-group");
 
-  if (this.mode === 'additive') {
+  if (this.mode === "additive") {
     if (isActive) {
       this._activeFilters.splice(this._activeFilters.indexOf(btnGroup));
     } else {
       this._activeFilters.push(btnGroup);
     }
 
-    btn.classList.toggle('active');
+    btn.classList.toggle("active");
     this.shuffle.filter(this._activeFilters);
-
   } else {
     this._removeActiveClassFromChildren(btn.parentNode);
 
     var filterGroup;
     if (isActive) {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
       filterGroup = Shuffle.ALL_ITEMS;
     } else {
-      btn.classList.add('active');
+      btn.classList.add("active");
       filterGroup = btnGroup;
     }
 
@@ -135,42 +134,42 @@ bookList.prototype._handleFilterClick = function(evt) {
 };
 
 // Remove classes for active states
-bookList.prototype._removeActiveClassFromChildren = function(parent) {
+bookList.prototype._removeActiveClassFromChildren = function (parent) {
   var children = parent.children;
   for (var i = children.length - 1; i >= 0; i--) {
-    children[i].classList.remove('active');
+    children[i].classList.remove("active");
   }
 };
 
 // Sort By
 // Watching for the select box to change to run
-bookList.prototype.addSorting = function() {
-  var menu = document.querySelector('.sort-options');
+bookList.prototype.addSorting = function () {
+  var menu = document.querySelector(".sort-options");
   if (!menu) {
     return;
   }
-  menu.addEventListener('change', this._handleSortChange.bind(this));
+  menu.addEventListener("change", this._handleSortChange.bind(this));
 };
 
 // Sort By function Handler runs on change
-bookList.prototype._handleSortChange = function(evt) {
+bookList.prototype._handleSortChange = function (evt) {
   var value = evt.target.value;
   var options = {};
 
   function sortByDate(element) {
-    return element.getAttribute('data-created');
+    return element.getAttribute("data-created");
   }
 
   function sortByTitle(element) {
-    return element.getAttribute('data-title').toLowerCase();
+    return element.getAttribute("data-title").toLowerCase();
   }
 
-  if (value === 'date-created') {
+  if (value === "date-created") {
     options = {
       reverse: true,
       by: sortByDate,
     };
-  } else if (value === 'title') {
+  } else if (value === "title") {
     options = {
       by: sortByTitle,
     };
@@ -182,32 +181,31 @@ bookList.prototype._handleSortChange = function(evt) {
 // Searching the Data-attribute Title
 // Advanced filtering
 // Waiting for input into the search field
-bookList.prototype.addSearchFilter = function() {
-  var searchInput = document.querySelector('.shuffle-search');
+bookList.prototype.addSearchFilter = function () {
+  var searchInput = document.querySelector(".shuffle-search");
   if (!searchInput) {
     return;
   }
-  searchInput.addEventListener('keyup', this._handleSearchKeyup.bind(this));
+  searchInput.addEventListener("keyup", this._handleSearchKeyup.bind(this));
 };
 
 // Search function Handler to search list
-bookList.prototype._handleSearchKeyup = function(evt) {
-  var searchInput = document.querySelector('.shuffle-search');
+bookList.prototype._handleSearchKeyup = function (evt) {
+  var searchInput = document.querySelector(".shuffle-search");
   var searchText = evt.target.value.toLowerCase();
 
   // Check if Search input has value to toggle class
   if (searchInput && searchInput.value) {
-    $('.catalog-search').addClass('input--filled');
+    $(".catalog-search").addClass("input--filled");
   } else {
-    $('.catalog-search').removeClass('input--filled');
+    $(".catalog-search").removeClass("input--filled");
   }
 
-  this.shuffle.filter(function(element, shuffle) {
-
+  this.shuffle.filter(function (element, shuffle) {
     // If there is a current filter applied, ignore elements that don't match it.
     if (shuffle.group !== Shuffle.ALL_ITEMS) {
       // Get the item's groups.
-      var groups = JSON.parse(element.getAttribute('data-groups'));
+      var groups = JSON.parse(element.getAttribute("data-groups"));
       var isElementInCurrentGroup = groups.indexOf(shuffle.group) !== -1;
 
       // Only search elements in the current group
@@ -216,7 +214,7 @@ bookList.prototype._handleSearchKeyup = function(evt) {
       }
     }
 
-    var titleElement = element.querySelector('.book-item_title');
+    var titleElement = element.querySelector(".book-item_title");
     var titleText = titleElement.textContent.toLowerCase().trim();
 
     return titleText.indexOf(searchText) !== -1;
@@ -224,6 +222,6 @@ bookList.prototype._handleSearchKeyup = function(evt) {
 };
 
 // Wait till dom load to start the Shuffle js funtionality
-document.addEventListener('DOMContentLoaded', function() {
-  window.book_list = new bookList(document.getElementById('grid'));
+document.addEventListener("DOMContentLoaded", function () {
+  window.book_list = new bookList(document.getElementById("grid"));
 });
